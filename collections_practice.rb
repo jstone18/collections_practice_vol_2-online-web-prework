d7f08d7c9e774af6ad7ac0aeb02f14e155c434b4
Jason Stone <jason.stone0808@gmail.com>
2018-11-16 06:07:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,3 +62,17 @@ def find_cool(array)
   end
   container
 end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
