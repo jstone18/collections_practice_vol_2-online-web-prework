fb622f13cdc0bc2c5a08f3c4d5385aeae98240a3
Jason Stone <jason.stone0808@gmail.com>
2018-11-16 03:46:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,8 @@
 # your code goes here
 
 def begins_with_r(array)
-  array.each do |word|
-    if word.map.start_with?("R")
+  array.map do |word|
+    if word.start_with?("R")
       true
     else
       false
